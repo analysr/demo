22d5c8f9ef991bc79be393452530f6e9b17f6112
Cédric JUNG <cedced19@gmail.com>
2021-09-23 23:56:40 +0200

    Add demo-import-events/periods

---
+++ demo-import-periods.r
@@ -0,0 +1,32 @@
+# On charge la version d'AnalysR qui nous interesse
+remove.packages("analysr")
+detach("package:analysr", unload = TRUE)
+devtools::install_github("analysr/analysr@75efef9")
+# '75efef9' numéro pour identifier le commit (en effet ce code est ammené à évoluer)
+.rs.restartR()
+
+# Fonction d'affichage de l'environement
+show_env <- function(env) {
+  sapply(ls(env), function(x) get(x, envir = env))
+}
+
+
+library(analysr)
+library(tidyverse)
+
+# On crée un pourrait remettre à 0 l'environnement
+# mais cela est déjà fait au chargement du package
+# setup_new_env()
+
+
+setup_new_env()
+
+show_env(analysr_env)
+
+import_periods_csv("./csv/import_periods_csv/to_import_1.csv",stat_unit="PERSON",begin="BEGIN",end="END",tag="DESCRIPTION",optional_data="LOCATION")
+
+show_env(analysr_env)
+
+import_periods_csv("./csv/import_periods_csv/to_import_2.csv",stat_unit="PERSON",begin="BEGIN",end="END",tag="DESCRIPTION")
+
+show_env(analysr_env)
