9a388a31d19a4762633f2fdcfc146c6651b5ea40
Cédric JUNG <cedced19@gmail.com>
2021-09-26 13:13:33 +0200

    Change demo

--- demo-new-request.R
+++ demo-new-request.R
@@ -1,14 +1,15 @@
+# On charge la version d'AnalysR qui nous interesse
+remove.packages("analysr")
+devtools::install_github("analysr/analysr@691a4c1")
+# '75efef9' numéro pour identifier le commit (en effet ce code est ammené à évoluer)
+.rs.restartR()
+rm(list=ls())
+
 # Fonction d'affichage de l'environement
+
 show_env <- function(env) {
   sapply(ls(env), function(x) get(x, envir = env))
 }
-
-# On charge la version d'AnalysR qui nous interesse
-remove.packages("analysr")
-devtools::install_github("analysr/analysr@75efef9")
-# '75efef9' numéro pour identifier le commit (en effet ce code est ammené à évoluer)
-.rs.restartR()
-
 
 library(analysr)
 library(tidyverse)
@@ -16,11 +17,21 @@ library(tidyverse)
 # On crée un pourrait remettre à 0 l'environnement
 # mais cela est déjà fait au chargement du package
 # setup_new_env()
+setup_new_env()
+
+show_env(analysr_env)
 
 load_env_csv("./csv/before/before/")
 
 show_env(analysr_env)
 
-model_filtred = analysr_env %>% observed(Temperture > 38.5) %>% at_most(15*days) %>% before("Surgery")
+result <- (
+  analysr_env
+  %>% observed(Temperature > 38.5)
+  %>% at_most(15 * days)
+  %>% before("Surgery")
+)
 
-show_env(model_filtred)
+show_env(analysr_env)
+
+print(result)
