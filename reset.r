fcb941d8a461a254ff97e1d92fa0e2a51a4e646a
Cédric JUNG <cedced19@gmail.com>
2021-09-26 23:41:50 +0200

    Change reset

--- reset.r
+++ reset.r
@@ -1,8 +1,8 @@
 # On charge la version d'AnalysR qui nous intéresse
 remove.packages("analysr")
 detach("package:analysr", unload = TRUE)
-devtools::install_github("analysr/analysr@886f82e")
-# '75efef9' numéro pour identifier le commit
+devtools::install_github("analysr/analysr")
+# '@75efef9' numéro pour identifier le commit
 rm(list=ls())
 .rs.restartR()
 
